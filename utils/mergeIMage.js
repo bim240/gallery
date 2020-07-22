21bd56d8ca1c43a6b78ee7f673385c8dc7f94e47
bim240 <bimlendu357@gmail.com>
2020-07-22 11:21:54 +0530

    tried merging images

--- utils/mergeIMage.js
+++ utils/mergeIMage.js
@@ -1,28 +1,33 @@
-const mergeImages = require("merge-images");
 const fs = require("fs");
-const { Canvas, Image } = require("canvas");
-const { decode } = require("node-base64-image");
+const jimp = require("jimp");
 
 const handleMergeImage = async (imageName) => {
   try {
     console.log(imageName, "imagename");
-    var mergedImage = await mergeImages(
-      ["http://localhost:3000/images/frame/frame.svg"],
-      {
-        Canvas: Canvas,
-        Image: Image,
-      }
-    );
-    fs.writeFile(
-      __dirname + "/../public/uploads/out.png",
-      mergedImage,
-      "base64",
-      function (err) {
-        if (err) console.log(err);
-        console.log("written image");
-      }
-    );
-    // return imageFile;
+    var images = [
+      "http://localhost:3000/images/frame/frame.svg",
+      "http://localhost:3000/uploads/1595325994214-alien.svg",
+    ];
+
+    var jimps = [];
+    for (var i = 0; i < images.length; i++) {
+      console.log("having fun");
+      jimps.push(jimp.read(images[i]));
+    }
+
+    Promise.all(jimps)
+      .then(function (data) {
+        return Promise.all(jimps);
+      })
+      .then(function (data) {
+        data[0].resize(512, 512);
+        data[0].composite(data[1], 0, 0);
+
+        data[0].write("../public/test.png", function () {
+          console.log("wrote the image");
+          return true;
+        });
+      });
   } catch (error) {
     console.log(error);
   }
